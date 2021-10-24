ac6c17920e702ed189ff94840f824da8b4b2c173
JoaoVGStahl <jvitorstahl@hotmail.com>
2021-10-24 14:00:54 -0300

    Scripts banco atualizados

--- Banco Dados/Procedures/Procedure_Gerencia_Usuario.sql
+++ Banco Dados/Procedures/Procedure_Gerencia_Usuario.sql
@@ -1,4 +1,52 @@
-CREATE OR ALTER PROCEDURE Gerencia_Usuario
+/****** Object:  StoredProcedure [dbo].[Gerencia_Veiculo]    Script Date: 24/10/2021 13:58:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE OR ALTER PROCEDURE [dbo].[Gerencia_Veiculo]
+(
+@Flag int,
+@idCarro int = null,
+@Placa varchar(7) = null,
+@Tipo varchar(20) = null,
+@Marca varchar(25) = null,
+@Status int = null
+)
+AS 
+DECLARE
+@idTipo int,
+@idMarca int
+-- 0 SALVAR/EDITAR 
+	IF(@Flag = 0)
+	BEGIN
+		IF(@idCarro IS NOT NULL)
+		BEGIN
+			UPDATE tb_carro SET status=0 WHERE id_carro=@idCarro
+		END			
+		SET @idTipo = (SELECT id_automovel FROM tb_automovel WHERE automovel=@Tipo)
+		SET @idMarca = (SELECT M.id_marca FROM tb_marca AS M INNER JOIN tb_automovel AS A ON M.automovel_id = A.id_automovel AND A.id_automovel=@idTipo AND M.marca=@Marca)
+		INSERT INTO tb_carro (placa,marca_id,tipo_id,status) VALUES(@Placa,@idMarca,@idTipo,@Status)
+	END	
+-- 1 == EXCLUIR VEICULO
+	IF(@Flag = 1)
+	BEGIN
+		UPDATE tb_carro SET status=0 WHERE id_carro=@idCarro
+	END
+GO
+
+
+/****** Object:  StoredProcedure [dbo].[Gerencia_Usuario]    Script Date: 24/10/2021 13:58:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[Gerencia_Usuario]
 @Flag INT,
 @Id INT = null,
 @Login VARCHAR(32) = NULL,
@@ -22,3 +70,5 @@ AS
 	   UPDATE tb_usuario SET status = 0 WHERE id_usuario = @Id;
 	END
 GO
+
+
--- Banco Dados/Procedures/Select_marca.sql
+++ Banco Dados/Procedures/Select_marca.sql
@@ -1,3 +1,36 @@
-SELECT TOP 1 marca[Marca], COUNT (marca) as Quantidade from tb_marca as M INNER JOIN tb_carro C ON C.marca_id = M.id_marca INNER JOIN tb_ticket T ON T.carro_id = C.id_carro GROUP BY marca ORDER BY Quantidade desc
+/****** Object:  StoredProcedure [dbo].[Adicionar_Marcas]    Script Date: 24/10/2021 13:56:35 ******/
+SET ANSI_NULLS ON
+GO
 
-SELECT TOP 1 automovel[Tipo], COUNT (automovel) as Quantidade from tb_automovel as A INNER JOIN tb_carro C ON C.tipo_id = A.id_automovel INNER JOIN tb_ticket T ON T.carro_id = C.id_carro GROUP BY automovel ORDER BY Quantidade desc
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[Adicionar_Marcas]
+@Flag INT,
+@idMarca int,
+@Marca VARCHAR(25) = NULL,
+@idTipo INT = NULL,
+@Tipo VARCHAR(20) = NULL,
+@Status SMALLINT = null
+AS
+--Flag 1 = Adicionar Marca
+   IF(@Flag = 1)
+   BEGIN
+      SET @idTipo = (SELECT id_automovel FROM tb_automovel WHERE automovel=@Tipo);
+      INSERT INTO tb_marca(marca,automovel_id,status) VALUES (@Marca,@idTipo,1);
+   END
+-- Flag 2 = Alterar Marca
+    IF(@Flag = 2)
+	BEGIN
+	   SET @idMarca = (SELECT id_marca FROM tb_marca INNER JOIN tb_automovel ON tb_marca.automovel_id = tb_automovel.id_automovel AND tb_automovel.id_automovel=@idTipo AND tb_marca.marca=@Marca)
+	   UPDATE tb_marca SET marca = @Marca, automovel_id = @idTipo, status = @Status WHERE id_marca = @idMarca; 
+	END
+-- Flag 3 = Deletar marca
+	IF(@Flag = 3)
+	BEGIN
+	   SET @idMarca = (SELECT id_marca FROM tb_marca INNER JOIN tb_automovel ON tb_marca.automovel_id = tb_automovel.id_automovel AND tb_automovel.id_automovel=@idTipo AND tb_marca.marca=@Marca)
+	   UPDATE tb_marca SET status = 0 WHERE id_marca = @idMarca;
+	END
+GO
+
+
