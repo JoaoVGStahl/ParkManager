cd6a7f6a40843f81062475bab5849f3f7c536f5c
JoaoVGStahl <jvitorstahl@hotmail.com>
2021-10-24 14:39:44 -0300

    Codigo Tela Login refatorado

--- Teste/Formulários/FrmTelaLogin.cs
+++ Teste/Formulários/FrmTelaLogin.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Data;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace Teste
 {
@@ -11,7 +13,6 @@ namespace Teste
         public FrmTelaLogin()
         {
             InitializeComponent();
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -21,101 +22,97 @@ namespace Teste
             Frm.Dispose();
             this.Dispose();
             Application.Exit();
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string usuario = txtUsuario.Text, senha = txtSenha.Text;
-            //Login Usuário Root!
-            if (usuario == Properties.Settings.Default.UserRoot && senha == Properties.Settings.Default.SenhaRoot)
-            {
-                Globais.IdUsuario = 1;
-                Globais.Login = Properties.Settings.Default.UserRoot;
-                Globais.Nivel = 3;
-                Globais.UserStatus = 1;
-                FrmTelaConfig Form = new FrmTelaConfig(this);
-                this.Hide();
-                Form.ShowDialog();
-                return;
-            }
-            if (usuario == "" || senha == "")
-            {
-                CaixaAlerta("Preencha todos os campos!", "Falha no login!");
-                txtUsuario.Focus();
-                return;
-            }
-            else
-            {
-                //Query P/ Consulta Banco
-                string query = @"
-                SELECT 
-                    id_usuario,login,nivel,status 
-                FROM 
-                    tb_usuario
-                WHERE 
-                    login='" + usuario + "' AND senha='" + senha + "'";
-                //Retorno do Banco em um DataTable!
-                DataTable dt = new DataTable();
-                try
-                {
-                    dt = banco.QueryBancoSql(query);
-                    //Verifica se a consulta retornou algum valor!
-                    if (dt.Rows.Count > 0)
-                    {
-                        //Verifica se o Usuário está Ativo!
-                        int status = Convert.ToInt32(dt.Rows[0].ItemArray[3]);
-                        if (status > 0)
-                        {
-                            //Atribui os valores do Usuario para Variaveis globais!
-                            PreencherGlobais(dt);
-                            Globais.RegistrarLog(Globais.Login + " Efetuou Login.");
-                            this.Hide();
-                            Frm.ShowDialog();
+            EfetuarLogin();
+        }
 
+        private void EfetuarLogin()
+        {
+            DataTable dt = new DataTable();
+            string usuario = txtUsuario.Text, senha = txtSenha.Text;
+            if (usuario != "" && senha != "")
+            {
+                if (usuario != Properties.Settings.Default["UserRoot"].ToString() && senha != Properties.Settings.Default["SenhaRoot"].ToString())
+                {
+                    try
+                    {
+                        List<SqlParameter> sp = new List<SqlParameter>()
+                        {
+                            new SqlParameter(){ParameterName="@Flag", SqlDbType = SqlDbType.Int, Value = 4},
+                            new SqlParameter(){ParameterName="@Login", SqlDbType = SqlDbType.VarChar, Value = usuario},
+                            new SqlParameter(){ParameterName="@Senha", SqlDbType = SqlDbType.VarChar, Value = senha}
+                        };
+                        dt = banco.InsertData("dbo.Gerencia_Usuario", sp);
+                        if (dt.Rows.Count > 0)
+                        {
+                            int status = Convert.ToInt32(dt.Rows[0]["Status"].ToString());
+                            if (status != 0)
+                            {
+                                PreencherGlobais(dt);
+                                Globais.RegistrarLog(Globais.Login + " Efetuou Login.");
+                                AbrirForm();
+                            }
+                            else
+                            {
+                                txtSenha.Clear();
+                                txtUsuario.Select();
+                                MessageBox.Show("Este usuário está inativo, contate o administrador do sistema!", "Falha no login!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
                         }
                         else
                         {
-                            CaixaAlerta("Este usuário está inativo, contate o administrador do sistema!", "Falha no login!");
                             txtSenha.Clear();
                             txtUsuario.Select();
+                            MessageBox.Show("Usuário e/ou senha incorretos ou não existem!", "Falha no login!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
                         }
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        CaixaAlerta("Usuário e/ou senha incorretos ou não existem!", "Falha no login!");
-                        txtSenha.Clear();
-                        txtUsuario.Select();
+                        MessageBox.Show(ex.Message, "Falha no login!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
-                catch (Exception ex)
+                else if (usuario == Properties.Settings.Default["UserRoot"].ToString() && senha == Properties.Settings.Default["SenhaRoot"].ToString())
                 {
-                    MessageBox.Show(ex.Message, "Falha no login!");
+                    Globais.IdUsuario = 1;
+                    Globais.Login = usuario;
+                    Globais.Nivel = 3;
+                    Globais.UserStatus = 1;
+                    FrmTelaConfig Form = new FrmTelaConfig(this);
+                    AbrirForm();
                 }
-                finally
-                {
-                    dt.Dispose();
-                }
-
             }
+            else
+            {
+                txtUsuario.Focus();
+                MessageBox.Show("Preencha todos os campos!", "Falha no login!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            dt.Dispose();
 
         }
+
+        private void AbrirForm()
+        {
+            this.Hide();
+            Frm.ShowDialog();
+        }
+
         private void PreencherGlobais(DataTable dt)
         {
-            Globais.IdUsuario = Convert.ToInt32(dt.Rows[0].ItemArray[0]);
-            Globais.Login = Convert.ToString(dt.Rows[0].ItemArray[1]);
-            Globais.Nivel = Convert.ToInt32(dt.Rows[0].ItemArray[2]);
-            Globais.UserStatus = Convert.ToInt32(dt.Rows[0].ItemArray[3]);
+            Globais.IdUsuario = Convert.ToInt32(dt.Rows[0]["ID"]);
+            Globais.Login = dt.Rows[0]["Login"].ToString();
+            Globais.Nivel = Convert.ToInt32(dt.Rows[0]["Nivel"]);
+            Globais.UserStatus = Convert.ToInt32(dt.Rows[0]["Status"]);
             Frm.lblUsername.Text = Globais.Login;
         }
-        private void CaixaAlerta(string texto, string titulo)
-        {
-            MessageBox.Show(texto, titulo, MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
+
         private void FrmTelaLogin_Load(object sender, EventArgs e)
         {
             //Registra no Log que o sistema foi Inicializado
-            Globais.RegistrarLog("Sistema foi Inicializado");
+            Globais.RegistrarLog("Sistema foi Inicializado.");
         }
 
         private void txtUsuario_KeyPress(object sender, KeyPressEventArgs e)
@@ -125,6 +122,14 @@ namespace Teste
             if (!(caracterespermitidos.Contains(e.KeyChar.ToString().ToUpper())) && !(e.KeyChar == (char)Keys.Back))
             {
                 e.Handled = true;
+            }
+        }
+
+        private void FrmTelaLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnEntrar.PerformClick();
             }
         }
     }
