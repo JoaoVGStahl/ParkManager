4c054e29d737639c90e661d9cd93b0e94156355a
João Vitor Girardi <Jvitorstahl@hotmail.com>
2021-11-17 15:21:23 -0300

    Add class Estacionamento

--- Teste/Classes/Estacionamento.cs
+++ Teste/Classes/Estacionamento.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Teste.Classes
 {
-    class Estacionamento
+    public static class Estacionamento
     {
+        public static string cnpj { get; set; }
+
+        public static string razao_social { get; set; }
+
+        public static string endereco { get; set; }
+
+        public static string bairro { get; set; }
+
+        public static int numero { get; set; }
+
+        public static string cidade { get; set; }
+
+        public static string cep { get; set; }
+
+        public static string telefone { get; set; }
+
+        public static TimeSpan tolerancia { get; set; }
+
+        public static decimal valor_hr { get; set; }
+
+        public static decimal valor_min { get; set; }
+
+        public static decimal valor_unico { get; set; }
+
+        public static string caminho_foto_padrao { get; set; }
+
     }
 }
